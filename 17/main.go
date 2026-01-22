3211a9e46c606f7b64934cecbb46e29bda6391ea
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2026-01-22 19:34:51 +0500

    17

--- 17/main.go
+++ 17/main.go
@@ -3,19 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(binarySearch([]int{1, 2, 3, 4, 5}, 3))
-	fmt.Println(binarySearch([]int{1, 2, 3, 4, 5}, -2))
-	fmt.Println(binarySearch([]int{1, 2, 3, 4, 5}, 6))
+	fmt.Println(binarySearch([]int{1, 2, 3, 4, 5}, 3))  // 2
+	fmt.Println(binarySearch([]int{1, 2, 3, 4, 5}, -2)) // -1
+	fmt.Println(binarySearch([]int{1, 2, 3, 4, 5}, 6))  // -1
 }
 
 func binarySearch(nums []int, item int) int {
 	low, high := 0, len(nums)-1
+
 	for low <= high {
 		mid := (low + high) / 2
 		guess := nums[mid]
 
 		if guess == item {
-			return item
+			return mid
 		}
 		if guess > item {
 			high = mid - 1
@@ -23,5 +24,6 @@ func binarySearch(nums []int, item int) int {
 			low = mid + 1
 		}
 	}
+
 	return -1
 }
